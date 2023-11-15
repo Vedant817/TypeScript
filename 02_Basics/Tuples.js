aedd586386830484ab7c4ff5b0d45515d1e16241
Vedant Mahajan <vedantmahajan271@gmail.com>
2023-11-15 18:24:39 +0530

    Understanding Tuples and there functions.

---
+++ 02_Basics/Tuples.js
@@ -0,0 +1,2 @@
+var user = [31, "VM"];
+var user2 = ["Vedant", 1, true]; //? Can't change the order of the variables being entered.
---
+++ 02_Basics/Tuples.ts
@@ -0,0 +1,3 @@
+const user: (string | number) [] = [31,"VM"]
+const user2 : [string,number,boolean] = ["Vedant", 1, true] //? Can't change the order of the variables being entered.
+user2.push(true)
